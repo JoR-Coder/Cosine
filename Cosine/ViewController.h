458feb2ce1f7fa0e9f4a34c7dfacf78e5db73463
Jyrki Rajala <jyrki.rajala@gmail.com>
2014-06-02 10:47:17 +0200

    Initial Commit

---
+++ Cosine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cosine
+//
+//  Created by Jyrki Rajala on 2014-06-02.
+//  Copyright (c) 2014 Jyrki Rajala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CosineTests/CosineTests.m
@@ -0,0 +1,34 @@
+//
+//  CosineTests.m
+//  CosineTests
+//
+//  Created by Jyrki Rajala on 2014-06-02.
+//  Copyright (c) 2014 Jyrki Rajala. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CosineTests : XCTestCase
+
+@end
+
+@implementation CosineTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
